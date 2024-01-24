fa98592205f188e7e5747b153cab337c79439dbb
kawamataryo <ryo.kawamata0425@gmail.com>
2024-01-24 19:59:09 +0900

    🐛 fixed unnecessary permission requests

--- src/lib/domHelpers.ts
+++ src/lib/domHelpers.ts
@@ -1,13 +1,3 @@
-import type {
-  ProfileView,
-  ViewerState,
-} from "@atproto/api/dist/client/types/app/bsky/actor/defs";
-import van from "vanjs-core";
-import { BskyUserCell, type UserCellBtnLabel } from "./components/BskyUserCell";
-import { NotFoundCell } from "./components/NotFoundCell";
-import { ReloadButton } from "./components/ReloadBtn";
-import type { BSKY_USER_MATCH_TYPE } from "./constants";
-
 export const getUserCells = ({
   queryParam,
   filterInsertedElement,
@@ -29,63 +19,10 @@ export const getUserCells = ({
   return Array.from(userCells);
 };
 
-export const insertReloadEl = (clickAction: () => void) => {
-  const lastInsertedEl = Array.from(
-    document.querySelectorAll(".bsky-user-content"),
-  ).at(-1);
-  van.add(lastInsertedEl.parentElement, ReloadButton({ clickAction }));
-};
-
-export const removeReloadEl = () => {
-  const reloadEl = document.querySelectorAll(".bsky-reload-btn-wrapper");
-  for (const el of reloadEl) {
-    el.remove();
-  }
-};
-
 export const getAccountNameAndDisplayName = (userCell: Element) => {
   const [avatarEl, displayNameEl] = userCell.querySelectorAll("a");
   const twAccountName = avatarEl?.getAttribute("href")?.replace("/", "");
   const twAccountNameRemoveUnderscore = twAccountName.replaceAll("_", ""); // bsky does not allow underscores in handle, so remove them.
   const twDisplayName = displayNameEl?.textContent;
   return { twAccountName, twDisplayName, twAccountNameRemoveUnderscore };
-};
-
-export const insertBskyProfileEl = ({
-  dom,
-  profile,
-  statusKey,
-  btnLabel,
-  matchType,
-  addAction,
-  removeAction,
-}: {
-  dom: Element;
-  profile: ProfileView;
-  statusKey: keyof ViewerState;
-  btnLabel: UserCellBtnLabel;
-  matchType: (typeof BSKY_USER_MATCH_TYPE)[keyof typeof BSKY_USER_MATCH_TYPE];
-  addAction: () => Promise<void>;
-  removeAction: () => Promise<void>;
-}) => {
-  van.add(
-    dom.parentElement,
-    BskyUserCell({
-      profile,
-      statusKey,
-      btnLabel,
-      matchType,
-      addAction,
-      removeAction,
-    }),
-  );
-};
-
-export const insertNotFoundEl = (dom: Element) => {
-  van.add(dom.parentElement, NotFoundCell());
-};
-
-export const isOutOfTopViewport = (el: Element) => {
-  const rect = el.getBoundingClientRect();
-  return rect.top < 0;
 };
--- src/lib/searchBskyUsers.ts
+++ src/lib/searchBskyUsers.ts
@@ -3,9 +3,6 @@ import { isSimilarUser } from "~lib/bskyHelpers";
 import {
   getAccountNameAndDisplayName,
   getUserCells,
-  insertBskyProfileEl,
-  insertNotFoundEl,
-  insertReloadEl,
 } from "~lib/domHelpers";
 import { debugLog, isOneSymbol } from "~lib/utils";
 import type { BskyClient } from "./bskyClient";
@@ -62,86 +59,4 @@ export const searchBskyUser = async ({
   }
 
   return null;
-};
-
-export const searchBskyUsers = async ({
-  agent,
-  userCellQueryParam,
-}: {
-  agent: BskyServiceWorkerClient | BskyClient;
-  userCellQueryParam: string;
-}) => {
-  const userCells = getUserCells({
-    queryParam: userCellQueryParam,
-    filterInsertedElement: true,
-  });
-  debugLog(`userCells length: ${userCells.length}`);
-
-  let index = 0;
-
-  const targetAccounts = [] as ProfileView[];
-
-  // loop over twitter user profile cells and search and insert bsky user
-  for (const userCell of userCells) {
-    const { twAccountName, twDisplayName, twAccountNameRemoveUnderscore } =
-      getAccountNameAndDisplayName(userCell);
-
-    if (notFoundUserCache.has(twAccountName)) {
-      insertNotFoundEl(userCell);
-      continue;
-    }
-
-    const searchTerms = [twAccountNameRemoveUnderscore, twDisplayName];
-
-    let targetAccount = null;
-    let matchType = null;
-
-    // Loop over search parameters and break if a user is found
-    searchLoop: for (const term of searchTerms) {
-      // one symbol is not a valid search term for bsky
-      if (!term || isOneSymbol(term)) {
-        continue;
-      }
-      try {
-        const searchResults = await agent.searchUser({
-          term: term,
-          limit: 3,
-        });
-
-        for (const searchResult of searchResults) {
-          const { isSimilar: isUserFound, type } = isSimilarUser(
-            {
-              accountName: twAccountName,
-              accountNameRemoveUnderscore: twAccountNameRemoveUnderscore,
-              displayName: twDisplayName,
-            },
-            searchResult,
-          );
-
-          if (isUserFound) {
-            targetAccount = searchResult;
-            matchType = type;
-            break searchLoop; // Stop searching when a user is found
-          }
-        }
-      } catch (e) {
-        console.error(e);
-      }
-    }
-
-    // insert bsky profile or not found element
-    if (targetAccount) {
-      targetAccounts.push(targetAccount);
-    } else {
-      notFoundUserCache.add(twAccountName);
-    }
-
-    index++;
-
-    // if (process.env.NODE_ENV === "development" && index > 5) {
-    //   break
-    // }
-  }
-
-  return targetAccounts;
 };
