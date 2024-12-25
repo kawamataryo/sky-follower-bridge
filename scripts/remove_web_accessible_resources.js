8e6defeb92d65ff47d95d0e92a81ebc67f4687ca
kawamataryo <ba068082@gmail.com>
2024-12-25 21:27:40 +0900

    add: clean script

--- scripts/remove_web_accessible_resources.js
+++ scripts/remove_web_accessible_resources.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
+const { consola } = require('consola');
 
-const manifestPath = './build/chrome-mv3-prod/manifest.json';
+const manifestPaths = [
+  './build/chrome-mv3-prod/manifest.json',
+  './build/chrome-mv3-dev/manifest.json',
+  './build/firefox-mv3-prod/manifest.json',
+  './build/firefox-mv3-dev/manifest.json',
+];
 
-const manifestData = fs.readFileSync(manifestPath);
-
-const manifestObj = JSON.parse(manifestData);
-
-delete manifestObj['web_accessible_resources'];
-
-fs.writeFileSync(manifestPath, JSON.stringify(manifestObj, null, 2));
+for (const manifestPath of manifestPaths) {
+  const manifestData = fs.readFileSync(manifestPath);
+  const manifestObj = JSON.parse(manifestData);
+  delete manifestObj.web_accessible_resources;
+  fs.writeFileSync(manifestPath, JSON.stringify(manifestObj, null, 2));
+  consola.success(`Removed web_accessible_resources from ${manifestPath} `);
+}
