6b4d5071b8ed8870c0a83e6c16752cb7ad76899d
kawamataryo <ryo.kawamata0425@gmail.com>
2025-01-24 10:14:56 +0900

    feat: add tooltip to action button

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -10,26 +10,28 @@ const DeleteButton = ({
   onClick: () => void;
 }) => {
   return (
-    <button
-      type="button"
-      className="btn-outline w-7 h-7 border rounded-full flex items-center justify-center"
-      onClick={onClick}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="h-4 w-4"
+    <div className="tooltip" data-tip={chrome.i18n.getMessage("delete_user")}>
+      <button
+        type="button"
+        className="btn-outline w-7 h-7 border rounded-full flex items-center justify-center"
+        onClick={onClick}
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M6 18L18 6M6 6l12 12"
-        />
-      </svg>
-    </button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="h-4 w-4"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </button>
+    </div>
   );
 };
 
@@ -39,26 +41,28 @@ const ReSearchButton = ({
   onClick: () => void;
 }) => {
   return (
-    <button
-      type="button"
-      className="btn-outline w-7 h-7 border rounded-full flex items-center justify-center"
-      onClick={onClick}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="h-4 w-4"
+    <div className="tooltip" data-tip={chrome.i18n.getMessage("search_again")}>
+      <button
+        type="button"
+        className="btn-outline w-7 h-7 border rounded-full flex items-center justify-center"
+        onClick={onClick}
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
-        />
-      </svg>
-    </button>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="h-4 w-4"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+          />
+        </svg>
+      </button>
+    </div>
   );
 };
 
